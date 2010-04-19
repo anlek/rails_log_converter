edfc44a34dc4cc9e535cf7c1dc1bcb30e4fd2a47
Andrew Kalek <andrew.kalek@anlek.com>
2010-04-19 17:52:57 -0400

    Small improvement in code

--- lib/rails_log_converter/cli/execute.rb
+++ lib/rails_log_converter/cli/execute.rb
@@ -9,33 +9,45 @@ module RailsLogConverter
         def execute
           parse(ARGV).execute!
         end
+        
+        def setup_logger
+          ActiveRecord::Base.logger = Logger.new(Configuration.out_stream)
+          ActiveRecord::Base.logger.level = Logger::WARN
+        end
+        
+        def create_database(path)
+          ActiveRecord::Base.establish_connection(
+            :adapter => "sqlite3",
+            :database => path
+          )
+          UI.puts "Saving output file to #{path}"
+          ActiveRecord::Base.connection
+
+          ActiveRecord::Migration.verbose = Configuration.debug?
+          ActiveRecord::Migrator.migrate(File.expand_path(File.dirname(__FILE__) + "/../db"), nil)
+        end
+        
+        def close_database
+          ActiveRecord::Base.connection.disconnect!
+        end
       end
       
-      def setup_logger
-        ActiveRecord::Base.logger = Logger.new(Configuration.out_stream)
-        ActiveRecord::Base.logger.level = Logger::WARN
-      end
+      
             
       def create_database
-        setup_logger
+        CLI.setup_logger
         
         path = "#{directory_path}/#{database_name}"
         File.delete path if File.exists?(path)
         
-        ActiveRecord::Base.establish_connection(
-          :adapter => "sqlite3",
-          :database => path
-        )
-        UI.puts "Saving output file to #{path}"
-        ActiveRecord::Base.connection
-        
-        ActiveRecord::Migration.verbose = Configuration.debug?
-        ActiveRecord::Migrator.migrate(File.expand_path(File.dirname(__FILE__) + "/../db"), nil)
+        CLI.create_database(path)
       end
       
       def close_database
-        ActiveRecord::Base.connection.disconnect!
+        CLI.close_database
       end
+      
+      
       
       def execute!
         create_database
